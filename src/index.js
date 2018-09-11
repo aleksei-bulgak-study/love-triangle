d7c60ac6ce977e4f4971b6d161dce14164ec1223
aleksei.bulgak <eledon@bk.ru>
2018-09-11 23:42:48 +0300

    Love triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,33 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var countOfTriangles = 0;
+  if (Array.isArray(preferences) && preferences.length) {
+    preferences.forEach(function (element, index, array) {
+      lastIndex = searchTriangle(element, array)
+      if (lastIndex == index) {
+        countOfTriangles += 1;
+        cleanFoundTriangleValues(index, array);
+      }
+    });
+  }
+  return countOfTriangles;
 };
+
+function searchTriangle(nextIndex, array, currentDepth = 1, maxDepth = 3) {
+  if (nextIndex == null || nextIndex < 0 || array.length < nextIndex
+    || nextIndex == array[nextIndex - 1])
+    return -1;
+  if (currentDepth == maxDepth)
+    return nextIndex - 1;
+  return searchTriangle(array[nextIndex - 1], array, currentDepth + 1);
+}
+
+function cleanFoundTriangleValues(first, array, maxDepth = 3) {
+  var index = first;
+  for (i = 0; i < maxDepth; i++) {
+    nextIndex = array[index] - 1;
+    array[index] = null;
+    index = nextIndex;
+  }
+}
